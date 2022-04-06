88eca599d92e3b91b1be484b58804eb10dfdcb3c
nelsonic <nelson+github@dwyl.com>
2022-04-06 23:28:11 +0100

    add coveralls & ex_doc to test and publish #1

--- mix.exs
+++ mix.exs
@@ -6,8 +6,20 @@ defmodule Gogs.MixProject do
       app: :gogs,
       version: "0.1.0",
       elixir: "~> 1.13",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        c: :test,
+        coveralls: :test,
+        "coveralls.html": :test
+      ],
+      aliases: aliases(),
+      deps: deps(),
+      package: package(),
+      description: "Interface with a Gogs (Git) Server"
     ]
   end
 
@@ -21,8 +33,29 @@ defmodule Gogs.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+
+      # Check test coverage
+      {:excoveralls, "~> 0.14.3", only: :test},
+
+      # Create Documentation for publishing Hex.docs:
+      {:ex_doc, "~> 0.28", only: :dev},
+    ]
+  end
+
+  defp aliases do
+    [
+      c: ["coveralls.html"],
+      "cover.html": ["coveralls.html"],
+    ]
+  end
+
+  defp package() do
+    [
+      files: ~w(lib LICENSE mix.exs README.md),
+      name: "gogs",
+      licenses: ["GPL-2.0-or-later"],
+      maintainers: ["dwyl"],
+      links: %{"GitHub" => "https://github.com/dwyl/gogs"}
     ]
   end
 end
