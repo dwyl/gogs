b13c47a4c4b0dc272e33eb1f727d0c3883e902ef
nelsonic <nelson+github@dwyl.com>
2022-05-11 13:05:07 +0100

    100% coverage in "mock: false" #24

---
+++ test/httpoison_mock_test.exs
@@ -0,0 +1,30 @@
+defmodule HttPoisonMockTest do
+  use ExUnit.Case, async: true
+  # Mock function tests that work both when mock: true and false!
+
+  test "Gogs.HTTPoisonMock.get /raw/ returns 200" do
+    {:ok, %HTTPoison.Response{ status_code: status}} = 
+      Gogs.HTTPoisonMock.get("/raw/", "any-header")
+    assert status == 200
+  end
+
+  test "Gogs.HTTPoisonMock.get any url should return status 200" do
+    {:ok, %HTTPoison.Response{ status_code: status}} = 
+      Gogs.HTTPoisonMock.get("any", "any-header")
+    assert status == 200
+  end
+
+  test "Gogs.HTTPoisonMock.post any url should return status 200" do
+    {:ok, %HTTPoison.Response{ status_code: status, body: resp_body}} = 
+      Gogs.HTTPoisonMock.post("hi", Jason.encode!(%{name: "simon"}), "any-header")
+    assert status == 200
+    body_map = Jason.decode!(resp_body) |> Useful.atomize_map_keys()
+    assert body_map.full_name == "myorg/simon"
+  end
+
+  test "Gogs.HTTPoisonMock.delete any url should return status 200" do
+    {:ok, %HTTPoison.Response{status_code: status}} = 
+      Gogs.HTTPoisonMock.delete("any?url")
+    assert status == 200
+  end
+end
